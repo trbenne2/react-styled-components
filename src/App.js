66be53fbce4acd5d57e18eab7ea6423e861168c5
trbenne2 <tyler_bennett25@yahoo.com>
2020-12-13 16:39:55 -0500

    All pages added and pricing compoent

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import GlobalStyle from './globalStyles';
-import { Footer, Navbar } from './components';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/HomePage/Home';
+import Services from './pages/Services/Services';
+import Products from './pages/Products/Products';
+import SignUp from './pages/SignUp/SignUp';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
+import { Navbar, Footer } from './components';
 
 function App() {
 	return (
 		<Router>
 			<GlobalStyle />
+			<ScrollToTop />
 			<Navbar />
 			<Switch>
 				<Route path='/' exact component={Home} />
+				<Route path='/services' component={Services} />
+				<Route path='/products' component={Products} />
+				<Route path='/sign-up' component={SignUp} />
 			</Switch>
 			<Footer />
 		</Router>
